00e198b1780ff58ebd5c8b717fab1dc0dbcd14be
Melissa Hull <melissahull87@gmail.com>
2022-08-01 15:53:25 -0500

    change signin to close

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,24 @@
-function confirm()
-  {
+function confirm() {
     if (document.getElementById('userpassword').value ==
-        document.getElementById('passconfirm').value)
-        {
-          document.getElementById('check').style.color = 'green';
-          document.getElementById('check').innerHTML = '&#10004';
-        }
-        else
-        {
-          document.getElementById('check').style.color = 'red';
-          document.getElementById('check').innerHTML = '&#10060';
-        }
+        document.getElementById('passconfirm').value) {
+        document.getElementById('check').style.color = 'green';
+        document.getElementById('check').innerHTML = '&#10004';
+    }
+    else {
+        document.getElementById('check').style.color = 'red';
+        document.getElementById('check').innerHTML = '&#10060';
+    }
 }
 
 function toggle() {
     var toggle = document.getElementById("login-modal");
+    var close = document.getElementById("close");
+
     if (toggle.style.display === 'none') {
         toggle.style.display = 'block';
+        close.innerHTML = 'Close';
     } else {
         toggle.style.display = 'none';
+        close.innerHTML = 'Sign In';
     }
 }
